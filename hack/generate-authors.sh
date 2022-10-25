d64011f87f36dbdb054761ab13b557460716a2fb
LeRoy-M <lmwasela@gmail.com>
2022-10-25 10:58:04 -0700

    AUTHORS file and generator-authors.sh script updated

--- hack/generate-authors.sh
+++ hack/generate-authors.sh
@@ -1,13 +1,13 @@
 #usr/env/bin bash
-# This script auto-generates the project authors' names and email addresses
+# This script auto-generates the project authors' names and email addresses, and adds them to the 'AUTHORS' file
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
 {
 	cat <<- 'EOH'
-		# This file lists all the project contributors
-		# See the 'hack/generate-authors.sh' file for more info
+		# This file lists all the project contributors by name and email address
+		# See the 'hack/generate-authors.sh' file for more info on generating it
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
